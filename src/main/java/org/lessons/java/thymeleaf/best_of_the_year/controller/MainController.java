63045e13561c55b73e80f2ff5e8f9cd92bd16a5f
defendimattia <defendi.mattia92@gmail.com>
2025-07-08 14:29:08 +0200

    Add static attribute to model in controller showed in home.html

--- src/main/java/org/lessons/java/thymeleaf/best_of_the_year/controller/MainController.java
+++ src/main/java/org/lessons/java/thymeleaf/best_of_the_year/controller/MainController.java
@@ -1,13 +1,15 @@
 package org.lessons.java.thymeleaf.best_of_the_year.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("username", "Mattia");
         return "home";
     }
 
