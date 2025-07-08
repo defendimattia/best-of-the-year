ace9fce33508c5fff7f2ce7848dbf57a19e2d705
defendimattia <defendi.mattia92@gmail.com>
2025-07-08 14:56:11 +0200

    Add 2 javabean classes Movie and Song

---
+++ src/main/java/org/lessons/java/thymeleaf/best_of_the_year/model/Song.java
@@ -0,0 +1,33 @@
+package org.lessons.java.thymeleaf.best_of_the_year.model;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+
+    private String id;
+    private String title;
+
+    public Song() {
+    }
+
+    public Song(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setId(String newId) {
+        this.id = newId;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+}
