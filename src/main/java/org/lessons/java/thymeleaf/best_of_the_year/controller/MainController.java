1dbbeba9212c39f1cfa0c333e782091d58e5077a
defendimattia <defendi.mattia92@gmail.com>
2025-07-08 14:15:40 +0200

    Add MainController with home method and add empty home.html

---
+++ src/main/java/org/lessons/java/thymeleaf/best_of_the_year/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.lessons.java.thymeleaf.best_of_the_year.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+}
